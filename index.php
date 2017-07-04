dbf12155a53f6035e1c7c76b5493b2308175a217
杨帆 <38130740@qq.com>
2017-07-04 20:54:19 +0800

    模板简单套上了又不和足部，具体页面大家自己弄不行了讨论 --杨帆

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// 应用目录为当前目录
+
+define('APP_PATH', str_replace('\\', '/', __DIR__). '/');
+
+// 开启调试模式
+define('APP_DEBUG', true);
+
+define('APP_HOST', 'http://www.myf.com/');
+
+define('APP_PUBLIC',APP_HOST.'public/');
+
+// 加载框架文件
+require(APP_PATH . 'fastphp/Fastphp.php');
+
+// 加载配置文件
+$config = require(APP_PATH . 'config/config.php');
+
+// 实例化框架类
+(new Fastphp($config))->run();
